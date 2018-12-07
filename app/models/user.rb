5c053a30149270f52f509fa641ba6b8cbcb10561
peterchang2 <peterchang664@gmail.com>
2018-12-07 10:14:34 -0500

    added password validation, length requirement for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
+# User validation
 class User < ApplicationRecord
   include Authentication
   has_many :examples
   has_many :images
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length: { within: 6..40 },
+            on: :create
+  validates :password,
+            confirmation: true,
+            length: { within: 6..40 },
+            allow_blank: true,
+            on: :update
 end
