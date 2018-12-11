101e6cfe6aa1e42acb4f1436502f05ce7cf141e2
peterchang2 <peterchang664@gmail.com>
2018-12-11 14:53:37 -0500

    validation fix

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 
 class Image < ApplicationRecord
   belongs_to :user
-  validates :url, format: { with: %r{/.(gif|jpg|png)/Z}i, message: 'must be a URL for JPG or PNG image.' }
+  validates :url, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for JPG or PNG image.' }
 end
